bcb79f819a4a235a335e952c2a52d93450207426
AlexandreSalim <alexandresalim7@gmail.com>
2024-10-04 17:35:47 -0300

    acertos no css

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-P7FTBCRY.mjs";import"./chunk-VM4U5WZA.mjs";import"./chunk-2QWWHLSM.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-3BUM5RLJ.mjs";import"./chunk-VM4U5WZA.mjs";import"./chunk-2QWWHLSM.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
